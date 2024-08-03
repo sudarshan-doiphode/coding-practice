c44a85a5cbac6ec665686427a9b098e0137ad516
sudarshan-doiphode <iamsudarshandoiphode@gmail.com>
2024-08-03 18:44:57 +0530

    Star Pattern 1

--- src/pattern/StarPattern1.java
+++ src/pattern/StarPattern1.java
@@ -1,2 +1,12 @@
-package pattern;public class StarPattern1 {
+package pattern;
+
+public class StarPattern1 {
+    public static void main(String[] args) {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
